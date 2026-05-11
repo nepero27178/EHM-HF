61dc2e9b50d7435563c127fd53d1ee688f819814
nepero27178 <alegori2002@gmail.com>
2026-05-11 13:03:20 +0200

    progress on report; typofix in chaps 2,4

--- Project/src/labs/mott-localization/plot-normal-mott-boundary.jl
+++ Project/src/labs/mott-localization/plot-normal-mott-boundary.jl
@@ -16,7 +16,7 @@ include(LAB_ROOT * "/../../setup/graphic-setup.jl")
 include(LAB_ROOT * "/../../modules/methods-3D-plotting.jl")
 
 FilePathIn = "/home/nepero27178/Thesis/EHM-HF/Project/data/refined/Mode=rs/Setup=B[128]/Phase=Normal/RB=S_Syms=.csv"
-# FilePathOut = LAB_ROOT * "/normal-mott-boundary-uS.pdf"
+# FilePathOut = LAB_ROOT * "/normal-mott-boundary-tS.pdf"
 
 # P = Plot3D(FilePathIn;Print=true,xVar="δ",yVar="V",zVar="tS",cs=:tabcoolerrev)
 # H = P[1].H
@@ -25,7 +25,7 @@ FilePathIn = "/home/nepero27178/Thesis/EHM-HF/Project/data/refined/Mode=rs/Setup
 # yy = 2 ./ df.uS
 
 # ax = H.content[1]
-# text!(ax,0.1,7.0,text="Mott localization",color=tabblue,align=(:center,:center))
+# text!(ax,0.1,7.0,text="Mott localisation",color=tabblue,align=(:center,:center))
 # scatterlines!(ax,xx,yy,color=tabblue,label=L"$V_{\mathrm{c}}[\beta=100,\delta]$")
 # ylims!(ax,0.0,8.0)
 # axislegend(ax,position=:rb)
@@ -33,34 +33,34 @@ FilePathIn = "/home/nepero27178/Thesis/EHM-HF/Project/data/refined/Mode=rs/Setup
 
 # save(FilePathOut, H)
 
-# FilePathOut = LAB_ROOT * "/normal-mott-boundary-f.pdf"
+FilePathOut = LAB_ROOT * "/normal-mott-boundary-f.pdf"
 
-# P = Plot3D(FilePathIn;Print=true,xVar="δ",yVar="V",zVar="f",cs=:tabquieter)
-# H = P[1].H
-# df = filter(:V => x -> x==0.0, P[1].DF)
-# xx = vcat(-0.05,df.δ)
-# yy = vcat(4.93, 2 ./ df.uS)
-
-# ax = H.content[1]
-# text!(ax,0.1,7.0,text="Mott localization",color=:white,align=(:center,:center))
-# lines!(ax,xx,yy,color=:white,linestyle=(:dash,:dense),linewidth=1)
-# xlims!(ax,0.0,0.49)
-# ylims!(ax,0.0,8.0)
-
-# save(FilePathOut, H)
-
-FilePathOut = LAB_ROOT * "/normal-mott-boundary-s.pdf"
-
-P = Plot3D(FilePathIn;Print=true,xVar="δ",yVar="V",zVar="s",cs=:tabwarm)
+P = Plot3D(FilePathIn;Print=true,xVar="δ",yVar="V",zVar="f",cs=:tabquiet)
 H = P[1].H
 df = filter(:V => x -> x==0.0, P[1].DF)
 xx = vcat(-0.05,df.δ)
 yy = vcat(4.93, 2 ./ df.uS)
 
 ax = H.content[1]
-text!(ax,0.1,7.0,text="Mott localization",color=:white,align=(:center,:center))
-lines!(ax,xx,yy,color=:white,linestyle=(:dash,:dense),linewidth=1)
+text!(ax,0.1,7.0,text="Mott localisation",color=:black,align=(:center,:center))
+lines!(ax,xx,yy,color=:black,linestyle=(:dash,:dense),linewidth=1)
 xlims!(ax,0.0,0.49)
 ylims!(ax,0.0,8.0)
 
 save(FilePathOut, H)
+
+# FilePathOut = LAB_ROOT * "/normal-mott-boundary-s.pdf"
+
+# P = Plot3D(FilePathIn;Print=true,xVar="δ",yVar="V",zVar="s",cs=:tabwarm)
+# H = P[1].H
+# df = filter(:V => x -> x==0.0, P[1].DF)
+# xx = vcat(-0.05,df.δ)
+# yy = vcat(4.93, 2 ./ df.uS)
+
+# ax = H.content[1]
+# text!(ax,0.1,7.0,text="Mott localisation",color=:white,align=(:center,:center))
+# lines!(ax,xx,yy,color=:white,linestyle=(:dash,:dense),linewidth=1)
+# xlims!(ax,0.0,0.49)
+# ylims!(ax,0.0,8.0)
+
+# save(FilePathOut, H)
